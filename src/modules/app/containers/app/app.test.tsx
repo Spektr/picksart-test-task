dd0b75123e81963a069a765202ae71b2e58facb7
Alexander Morozov <as.morozov@ekoniva-apk.com>
2023-07-24 10:24:16 +0400

    remove src files

--- src/modules/app/containers/app/app.test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-test('fake test', () => {
-  expect(true).toBeTruthy();
-});
--- src/store/initial-state.ts
+++
@@ -1,8 +0,0 @@
-import {IGlobalState} from "../shared/interfaces/global-state";
-
-export const initialState: IGlobalState = {
-    zoom: 10,
-    isColorPickerEnabled: false,
-    pickedColor: null,
-    editor: null,
-};
